bcbb935d3191bc07b6155745e657be126ca777ee
AminAhantab <amin.ahantab@gmail.com>
2023-02-10 16:05:55 +0100

    backend-update

--- alphagamePublic.py
+++ alphagamePublic.py
@@ -76,7 +76,7 @@ def do_predict():
     print(result)
     response = jsonify({"output_string": str(result)})
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
     response.headers.add("Access-Control-Allow-Credentials", "true")
     
